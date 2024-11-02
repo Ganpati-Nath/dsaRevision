d469dc17cd59404451a7522cb35286c0c5ea7ca7
Ganpati <ganpatinath07@gmail.com>
2024-11-03 04:57:56 +0530

    dsa REVISION...

--- ArrayRevision/basics.cpp
+++ ArrayRevision/basics.cpp
@@ -12,13 +12,20 @@ int main(){
     // cout << "Array creation done..." << endl;
 
     //addressOf operator...
-    int arr[10];
+    // int arr[10];
     // cout << "Address/base address of arr is : " << &arr << endl;
     //you can also print address in case of an array without addressOf operator...
     // cout << "Address/base address of arr is : " << arr << endl;
 
     //sizeOf method...
     // cout << "Size of current array is : " << sizeof(arr) << endl;
+
+    //initialization of an array...
+    // int arr[] = {1, 2, 4, 5};
+    // int brr[4] = {1, 2, 4, 5};
+    // int crr[4] = {1, 2};
+    //error...
+    // int drr[2] = {1, 2, 4, 5};
 
     return 0;
 }
