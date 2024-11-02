36da4f3f1b280d5ff60fc4043600a760ba55ea74
Ganpati <ganpatinath07@gmail.com>
2024-11-03 04:32:49 +0530

    dsa REVISION...

---
+++ ArrayRevision/basics.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //creation of an array...
+    // int money[127];
+    // char alpha[26];
+    // bool flags[27];
+    // long num[20];
+
+    // cout << "Array creation done..." << endl;
+
+    return 0;
+}
